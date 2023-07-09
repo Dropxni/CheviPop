c18148016d72dcccad07372609d683c113504f93
Dropxni <xniwis@outlook.com>
2023-07-09 11:33:52 -0600

    git remote add origin https://github.com/Dropxni/TYPERMAN.git git branch -M main git push -u origin main

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  
+  firebase: {
+    projectId: 'typerman',
+    appId: '1:611752279888:web:c569b30255d2afbe6d7655',
+    storageBucket: 'typerman.appspot.com',
+    apiKey: 'AIzaSyAtNPUdkBRquFGIPjM5-VTnLRXtJc0sQ5c',
+    authDomain: 'typerman.firebaseapp.com',
+    messagingSenderId: '611752279888',
+    measurementId: 'G-KDCD4XV540',
+  },
+  production: true
+};
